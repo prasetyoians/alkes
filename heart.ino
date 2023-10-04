bed73f0e4e2ba52bf076acb23252fccb48146ff4
prasetyoians <prasetyoians@gmail.com>
2023-10-04 08:47:04 +0700

    komit kedua

--- heart.ino
+++ heart.ino
@@ -1,13 +1,47 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+
+#define REPORTING_PERIOD_MS     1000
 
 Adafruit_MPU6050 mpu;
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 
+
+PulseOximeter pox;
+
+uint32_t tsLastReport = 0;
+
+// Callback (registered below) fired when a pulse is detected
+void onBeatDetected()
+{
+    Serial.println("Beat!");
+}
+
+
+
 void setup() {
   Serial.begin(115200);
   // while (!Serial);
+
+  Wire.begin();
+
+  Serial.print("Initializing pulse oximeter..");
+
+    // Initialize the PulseOximeter instance
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+
+
+    
   Serial.println("MPU6050 OLED demo");
 
   if (!mpu.begin()) {
@@ -17,7 +51,7 @@ void setup() {
   }
   Serial.println("Found a MPU6050 sensor");
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally  
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for (;;)
@@ -28,11 +62,44 @@ void setup() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setRotation(0);
+
+     pox.setOnBeatDetectedCallback(onBeatDetected);
+
+
+
 }
 
 void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
+
+   pox.update();
+
+    // Asynchronously dump heart rate and oxidation levels to the serial
+    // For both, a value of 0 means "invalid"
+
+       if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("Heart rate:");
+        Serial.print(pox.getHeartRate());
+        Serial.print("bpm / SpO2:");
+        Serial.print(pox.getSpO2());
+        Serial.println("%");
+
+              
+
+        display.print("Heart rate: ");
+        display.print(pox.getHeartRate(), 1);
+        display.print(" bpm, ");
+        display.print("SPO2: ");
+        display.print(pox.getSpO2(), 1);
+        display.println(" %, ");
+        
+        tsLastReport = millis();
+
+
+    }
+
+
 
   display.clearDisplay();
   display.setCursor(0, 0);
@@ -48,7 +115,6 @@ void loop() {
   Serial.print(a.acceleration.z, 1);
   Serial.println(" m/s^2");
 
-
   display.println("Accelerometer - m/s^2");
   display.print(a.acceleration.x, 1);
   display.print(", ");
@@ -57,24 +123,26 @@ void loop() {
   display.print(a.acceleration.z, 1);
   display.println("");
 
-  Serial.print("Gyroscope ");
-  Serial.print("X: ");
-  Serial.print(g.gyro.x, 1);
-  Serial.print(" rps, ");
-  Serial.print("Y: ");
-  Serial.print(g.gyro.y, 1);
-  Serial.print(" rps, ");
-  Serial.print("Z: ");
-  Serial.print(g.gyro.z, 1);
-  Serial.println(" rps");
+  // Serial.print("Gyroscope ");
+  // Serial.print("X: ");
+  // Serial.print(g.gyro.x, 1);
+  // Serial.print(" rps, ");
+  // Serial.print("Y: ");
+  // Serial.print(g.gyro.y, 1);
+  // Serial.print(" rps, ");
+  // Serial.print("Z: ");
+  // Serial.print(g.gyro.z, 1);
+  // Serial.println(" rps");
 
-  display.println("Gyroscope - rps");
-  display.print(g.gyro.x, 1);
-  display.print(", ");
-  display.print(g.gyro.y, 1);
-  display.print(", ");
-  display.print(g.gyro.z, 1);
-  display.println("");
+  // display.println("Gyroscope - rps");
+  // display.print(g.gyro.x, 1);
+  // display.print(", ");
+  // display.print(g.gyro.y, 1);
+  // display.print(", ");
+  // display.print(g.gyro.z, 1);
+  // display.println("");
+
+  
 
   display.display();
   delay(100);
